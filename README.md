2d314e5fec08fab73727979ef9cc892fe359d447
Rohith Gilla <gillarohith1@gmail.com>
2018-06-02 18:52:11 +0530

    Adding Documentation

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # PyCon-Twitter-Bot
 
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+
+## Pre-requisites 
+1. Twitter Account
+    
+2. Modules required
+    * tweepy
+    * time
+    * django.utils.encoding
+    
+### To install required modules use these commands
+
+```bash
+pip install tweepy
+pip install django
+
+```
+
+### Creating twitter api credentials
+
+1. Goto the following [link] (https://apps.twitter.com/ "Twitter App").
+2. Fill the necessary details like :
+    * Name of the bot.
+    * Website name.
+    * Description.
+3. Goto Keys and tokens and generate your access tokens.
+
+
+### Using the tweepy api
+
+Authorise the app with tweepy api with consumer keys and access tokens.
+Now replace the credentials in the code with your credentials and search hashtags with
+the one which you wanted and enjoy the app up and running.
+
+
+
+### Hosting in heroku
+
+Use the following commands to host this app in heroku
+```bash
+heroku create
+git add .
+git commit -m "initial commit"
+git push heroku master
+heroku ps:scale worker=1
+heroku logs --tail
+```
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#Required imports
 
-import tweepy
-import time
+import tweepy #for twitter API
+import time   #for giving 15 seconds break between tweets
 from django.utils.encoding import smart_str
 
 # Twitter Access Tokens
@@ -16,21 +17,22 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweets=[]
-names=[]
-namesperson=[]
+tweets=[]  # For collection of all tweets it acts as db for tweets
+names=[]   # For collection of all user ids it acts as db for userids
+namesperson=[] # For collection of all usernames it acts as db for user names
 while True:
     for tweet in tweepy.Cursor(api.search,q="#PyConIndia2018").items(10):
         if tweet in tweets:
             pass
         else:
-            tweets.append(tweet.text)
-            if(tweet.author.id not in names):
+            tweets.append(tweet.text) #AAdding the tweet to db
+            if(tweet.author.id not in names): #Checking if the same person tweeted more than once
                 names.append(tweet.author.id)
                 namesperson.append(tweet.user.name)
 
             try:
                 #tweet.retweet()
+                #Retweet with reply feature implementaiton, using smart_str to convert unicode to string
                 if(smart_str(tweet.author.screen_name)!="BotGills"):
                     perfect = "RT @"+smart_str(tweet.author.screen_name)+" "+smart_str(tweet.text)
                     api.update_status(perfect)
@@ -71,7 +73,7 @@ while True:
                 pass
     for i in names:
         try:
-            api.create_friendship(i)
+            api.create_friendship(i) #Following the person who tweeted with #PyConIndia #PyConIndia2018
         except:
             pass
     time.sleep(15)
