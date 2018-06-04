afb0a31082e8ab3de91836987edbe4d1cb0b4e07
Rohith Gilla <gillarohith1@gmail.com>
2018-06-04 15:46:38 +0530

    PEP 8 validations

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweets=[]  # For collection of all tweets it acts as db for tweets
-names=[]   # For collection of all user ids it acts as db for userids
-namesperson=[] # For collection of all usernames it acts as db for user names
+tweets = []  # For collection of all tweets it acts as db for tweets
+names = []   # For collection of all user ids it acts as db for userids
+namesperson = []  # For collection of all usernames it acts as db for user names
 while True:
     for searchterm in searchterms:
         
-        for tweet in tweepy.Cursor(api.search,q=searchterm).items(10):
+        for tweet in tweepy.Cursor(api.search, q=searchterm).items(10):
             if tweet in tweets:
                 pass
             else:
-                tweets.append(tweet.text) #AAdding the tweet to db
-                if(tweet.author.id not in names): #Checking if the same person tweeted more than once
+                tweets.append(tweet.text)  # AAdding the tweet to db
+                if tweet.author.id not in names:  # Checking if the same person tweeted more than once
                     names.append(tweet.author.id)
                     namesperson.append(tweet.user.name)
 
                 try:
-                    #tweet.retweet()
-                    #Retweet with reply feature implementaiton, using smart_str to convert unicode to string
-                    if(smart_str(tweet.author.screen_name)!=volunteertwitterhandle):
+                    # tweet.retweet()
+                    # Retweet with reply feature implementaiton, using smart_str to convert unicode to string
+                    if smart_str(tweet.author.screen_name)!=volunteertwitterhandle:
                         perfect = "RT @"+smart_str(tweet.author.screen_name)+" "+smart_str(tweet.text)
                         api.update_status(perfect)
                         print("Retweeted")
@@ -41,7 +41,7 @@ while True:
                     pass
     for i in names:
         try:
-            api.create_friendship(i) #Following the person who tweeted with #PyConIndia #PyConIndia2018
+            api.create_friendship(i)  # Following the person who tweeted with #PyConIndia #PyConIndia2018
         except:
             pass
     time.sleep(15)
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='PyconfBot',
-    version='0.1',
+    version='0.2',
     author='Rohith Gilla',
     author_email='gillarohith1@gmail.com',
-    scripts=['fill with your detils'],
+    scripts=['config.py','main.py'],
     url='https://twitter.com/BotGills/',
     description='Official PyConIndia  Retweet bot.',
     long_description=open('README.md').read(),
