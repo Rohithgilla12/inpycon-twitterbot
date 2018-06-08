c7e4b4cf03c70dc50ab6962fb85f5c75df6487d1
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-06-08 16:08:15 +0530

    Changes made as requested
    - Edited commit history
    - Implemented pylint
    - Removed namepersons[]
    
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-consumer_key = "c9pXLroOFv2y9wr4oLZh6Wlov"
-consumer_secret = "GiRF7HfwvvFdtFUbPjaZmHs0sOq2tF9fWxah2FrDDkNAf28uHB"
-access_token = "749661321977819138-Jpoo3Y70JDWYJi5FR4Q0tC7c60x1CDu"
-access_token_secret = "248GI3AMavIAZ94xLxa5nmY4c8OjoD36yOLgGeXFbTWzM"
-volunteertwitterhandle = "vipulgupta2048"
+consumer_key = "Your Consumer Key here"
+consumer_secret = "Your Consumer Secret here"
+access_token = "Your Access Token here"
+access_token_secret = "Your Access Secret here"
+volunteertwitterhandle = "Your Twitter handle"
 # add search terms of your choice
-searchterms = ["#PyConIndia2018","#PyConIndia","@pyconindia"]
+searchterms = ["#PyConIndia2018", "#PyConIndia", "@pyconindia"]
