9cef64b5bb2f5b6e5cbe50c805af6510efb9854e
Rohith Gilla <gillarohith1@gmail.com>
2018-06-16 11:31:48 +0530

    Modifying according to review

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Script for execution of the bot
 import time  # for giving 15 seconds break between tweets
 import tweepy  # for twitter API
 from django.utils.encoding import smart_str
+twitter = "https://twitter.com"
 
 
 class Tweet:
@@ -21,25 +22,20 @@ class Tweet:
         return api
 
     def run(self):
-        tweets = []
-        names = []
         while True:
             for search_term in self.search_terms:
                 for tweet in tweepy.Cursor(self.auth().search, q=search_term).items(10):
-                    if tweet in tweets:
+                    if self.auth().get_status(tweet.id):
                         pass
                     else:
-                        tweets.append(tweet.text)
-                        if tweet.author.id not in names:
-                            names.append(tweet.author.id)
+                        self.follow_them(tweet)
                         self.tweet_and_like(tweet)
                         try:
                             if smart_str(tweet.author.screen_name) != self.auth().me().screen_name:
                                 self.auth().update_status(self.get_perfect(tweet))
                                 print("Re tweeted with comment of -" + tweet.author.screen_name)
                         except tweepy.TweepError:
-                            pass
-            self.follow_them(names)
+                            pass  # Already re tweeted
             time.sleep(20)
 
     @staticmethod
@@ -48,20 +44,18 @@ class Tweet:
             tweet.retweet()
             tweet.favorite()
         except tweepy.TweepError:
-            pass
+            pass  # Already re tweeted and liked
 
-    def follow_them(self, names):
-        for name in names:
-            try:
-                self.auth().create_friendship(name)
-            except tweepy.TweepError:
-                pass
+    def follow_them(self, tweet):
+        try:
+            self.auth().create_friendship(tweet.id)
+        except tweepy.TweepError:
+            pass  # Already a friend
 
     @staticmethod
     def get_perfect(self, tweet):
-        twitter = "https://twitter.com"
         url = twitter + "/" + tweet.author.screen_name + \
-                        "/" + "status" + "/" + str(tweet.id)
+              "/" + "status" + "/" + str(tweet.id)
         perfect = smart_str("#PyConIndia2018" + " " +
                             "#pyconindia" + " " +
                             "#PyConIndia \n" + url)
